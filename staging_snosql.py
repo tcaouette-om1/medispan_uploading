c7b7e047d53dcead4190c3adbf624281b444cb0f
tcaouette-om1 <82820698+tcaouette-om1@users.noreply.github.com>
2022-05-09 13:04:48 -0700

    snowsql

---
+++ staging_snosql.py
@@ -0,0 +1,17 @@
+import os, shlex, subprocess
+from subprocess import call
+# need to build main controller main()
+# this file is used to connect to snowsql client and run the files denoted by -f flag. these directories need to be updated for each new user.
+# change location of the files before running. check the files for complete setup.
+# files being passed - put_file.sql, medispan_create_tables.sql, medispan_copyinto.sql
+# scripts being used - medispan_use_db_create_tables.py, medispan_copyinto.py
+os.system('''snowsql -c my_conn -f /Users/tobiascaouette/Documents/put_file.sql  -f /Users/tobiascaouette/Documents/medispan_create_tables.sql -f /Users/tobiascaouette/Documents/medispan_copyinto.sql''')
+
+#os.system('''snowsql -c my_conn''')
+    
+
+    
+
+
+
+
